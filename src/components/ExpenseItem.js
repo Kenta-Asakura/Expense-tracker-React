c67bec5fef541d721cb0664e533bf46fd600ca50
kenta asakura <asakura.kenta963420@gmail.com>
2023-06-25 14:12:05 +0900

    Expenses.js component created which passes the date, title, and amount data as props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,7 +5,6 @@ function ExpenseItem({ date, title, amount }) {
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
-      {/* <div>{date.toLocaleString("en-US", { month: "long" })}</div> */}
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
