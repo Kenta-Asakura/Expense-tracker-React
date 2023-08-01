ad587e646ea3a2ede85b44639dd2087e8bd019db
kenta asakura <asakura.kenta963420@gmail.com>
2023-08-01 22:56:50 +0900

    alternative way and better way of using single state with prevState

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -15,27 +15,38 @@ const ExpenseForm = () => {
   const titleChangeHandler = (event) => {
     console.log(event.target.value); // logs the value
     // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value
+    // })
+    setUserInput((prevState) => {
+      return {
+        ...prevState, enteredTitle:event.target.value
+      }
     })
   };
 
   const amountChangeHandler = (event) => {
     console.log(event.target.value);
     // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value
+    // })
+    setUserInput((prevState) => {
+      return {
+        ...prevState, enteredAmount:event.target.value
+      }
     })
   };
 
   const dateChangeHandler = (event) => {
     console.log(event.target.value);
     // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value
+    setUserInput((prevState) => {
+      return {
+        ...prevState, enteredDate:event.target.value
+      }
     })
   };
 
